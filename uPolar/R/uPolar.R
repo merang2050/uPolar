3e22cfab6a18cd4c97e88d4eea675bb3d8e6d558
merang <ryg668@mocs.utc.edu>
2020-02-10 14:33:20 -0500

    update

--- uPolar/R/uPolar.R
+++ uPolar/R/uPolar.R
@@ -1,4 +1,4 @@
-#' @title  μPolar Visualization tool for time series of microfluidic microscopic images
+#' μPolar Visualization tool for time series of microfluidic microscopic images
 #'
 #' @description AN easy-to-use visualizing tool for Microfluidic based microscopy images to monitor cellular events in biomedical time-series data.
 #'
@@ -24,314 +24,299 @@
 #'
 #' @examples uPolar(df,1,2,3,4,0,7,391)
 #'
-#' @note   insrtall packages : dplyr , plotly ,RDocumentation
-library(dplyr)
-library(plotly)
-library(RDocumentation)
+#' @note   install packages : dplyr , plyr, plotly
 uPolar <- function(df,time_col,obj_col,area_col,dist_col,offset,c.Adjust,num_images){
 
-  print(" Load Data  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ")
-  # df.name <-  file.choose(new=FALSE)
-  #
-  # #df = read.csv(" filename + path ")                       # data name by given name and path
-  # df = read.csv(df.name)                                    # file by directory
-  # df= select(df,"time_num","total_objs","Est_area","dist")  #arrange feature in order
+  print(' Step1 : Loading data ')
 
+  t2d <- function (df, time_col,num_images){
 
-    ############ Convet Time to Degree (theta ) #############
-    t2d <- function (df, time_col,num_images){
+    df$theta = 'NA'
+    dg =0   # initilize degree
 
-      df$theta = NA
-      dg =0   # initilize degree
+    for  (i in 1 : nrow(df)){
 
-      for  (i in 1 : nrow(df)){
+      r1  = df[i, ];
+      r2 =  df[i+1, ];
 
-        r1  = df[i, ];
-        r2 =  df[i+1, ];
+      if (is.na(r2[,time_col])){
+        #if (is.na(r2$time_num)){  # set degree to zero for unavailable data
 
-        if (is.na(r2[,time_col])){
-          #if (is.na(r2$time_num)){  # set degree to zero for unavailable data
+        df$theta[i] = dg
+        dg = 0
 
-          df$theta[i] = dg
-          dg = 0
-        }
-        else if (r1[,time_col] < r2[,time_col]){
-          df$theta[i] = dg
-          dg= dg + (360/num_images)   # divid time to degree and add to next degree
-        }
-        else
-          df$theta[i] = dg
       }
 
-      print(' Step1 : Time converted to degree (theta) >>>>>>>>>>>>>> ')
-      return(df)
+      else if (r1[,time_col] < r2[,time_col]){
+
+        df$theta[i] = dg
+        dg= dg + (360/num_images)   # divid time to degree and add to next degree
+
+      }
+
+      else
+
+        df$theta[i] = dg
     }
 
-    ##### tronspose row to colunm ###################################################
-    cbind.fill <- function(...) {
-      df.tsps1 <- lapply(list(...),t)
-      df.tsps2 <- lapply(df.tsps1, as.data.frame)
-      return (data.frame(t(rbind.fill(df.tsps2))))
+    print(' Step2 : Time converted to degree (theta) ')
+    return(df)
+
+  }
+
+  cbind.fill <- function(...) {
+    df.tsps1 <- lapply(list(...),t)
+    df.tsps2 <- lapply(df.tsps1, as.data.frame)
+    return (data.frame(t(rbind.fill(df.tsps2))))
+  }
+
+
+  ##### Data format (time_col,objs_col,area_col,dist_col, theta_col)######
+  ##### Data format ( 1,        2,        3,        4,        5    )######
+
+
+  # Convert distance and theta to  order of time for plotly format
+  c2r <- function(df,time_col,objs_col,area_col,dist_col,theta_col,num_images ){
+
+    df.row = data.frame()
+    df.obj = data.frame()
+
+    max.r = max(df[,dist_col])        # max raduis
+    min.r = min(df[,dist_col])        # min raduis
+
+
+    if( area_col==0){   # if area colunm is unavailable
+      df.area = 0
+    }
+    else{
+      df.area = df[,area_col]
     }
 
 
-    ######## Convert distance and theta to  order of time for plotly format ########
-    c2r <- function(df,time_col,objs_col,area_col,dist_col,theta_col,num_images ){
+    for (k  in 1: num_images){
 
-      df.row = data.frame()
-      df.obj = data.frame()
+      df.t = df[df[,time_col]==k,]
 
-      max.r = max(df[,dist_col])        # max raduis
-      min.r = min(df[,dist_col])        # min raduis
+      objs = mean(df.t[,objs_col])   # Objects
+      objs = mean(df.t[,objs_col])   # Objects
+      raduis <- df.t[,dist_col]  # Distance
+      theta <-  df.t[,theta_col]       # Angle
+      df1 <- cbind(raduis,theta)
 
-
-      if( area_col==0){   # if area colunm is unavailable
-        df.area = 0
-      }
-      else{
-        df.area = df[,area_col]
-      }
-
-      for (k  in 1: num_images){
-
-        df.t = df[df[,time_col]==k,]
-
-        objs = mean(df.t[,objs_col])   # Objects
-        objs = mean(df.t[,objs_col])   # Objects
-        raduis <- df.t[,dist_col]  # Distance
-        theta <-  df.t[,theta_col]       # Angle
-        df1 <- cbind(raduis,theta)
-
-        #df.row = cbind.fill(df.row,df1,fill = NA)              ( use only for rowR pachage)
-        df.row = cbind.fill(df.row,df1)
-        df.obj = rbind(df.obj, objs)
-
-      }
-      #df.row  <- df.row [,-c(1)]     # remove first colunm    ( use only for rowR pachage)
-      df.obj= as.vector(t(df.obj))   # conver object data.frame to vector
-
-      print(' Step2 : Data Converted to Plotly format >>>>>>>>>>>>>> ')
-
-      return (list(v1=df.row,v2=df.obj,v3=df.area, v4=max.r,v5=min.r))
+      df.row = cbind.fill(df.row,df1)
+      df.obj = rbind(df.obj, objs)
 
     }
+    #df.row  <- df.row [,-c(1)]     # remove first colunm
+    df.obj= as.vector(t(df.obj))   # conver object data.frame to vector
+    print(' Step3 : Data Converted to Plotly format ')
 
-    ######################### Plot Function #########################
-    ipolar <- function (df,off_set,c.Adjust ){
+    return (list(v1=df.row,v2=df.obj,v3=df.area, v4=max.r,v5=min.r))
 
-      df.1= df$v1  # raduis and theta
-      df.2= df$v2  # total Object
-      df.3= df$v3  # area
-      max.area = median(df.3)
-      rls=c()
+  }
 
+  ##### tronspose row to colunm ###################################################
 
-      if ( missing(df)){
-        print('missing "data" entery')
-        break
-      }
-      else if (missing(off_set)){
-        off_set = 5;
-        print('offset : default (5)')
-      }
-      else if (missing(c.Adjust) | c.Adjust==0 | mean(df.3)==0 & c.Adjust==0  ){
-        c.Adjust = 0;
-        print(' Cell Size : default (5)')
-      }
-      else{
-        print(' Correct Entery')
-      }
+  p2p <- function (df,off_set,c.Adjust ){
 
-      print(' Step3: Preparing for Plot >>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    df.1= df$v1  # raduis and theta
+    df.2= df$v2  # total Object
+    df.3= df$v3  # area
+    max.area = median(df.3)
+    rls=c()
 
 
-      #####################  Generat Polar Plot #####################
-      p <- plot_ly(
-        type = 'scatterpolargl',
-        r = c((df$v5)-30, df$v4+off_set),
-        mode = 'markers_line'
+    if ( missing(df)){
+      print('missing "data" entery')
+    }
+    else if (missing(off_set)){
+      off_set = 5;
+      print('offset : 0 entery,  set to  5 (default) ')
+    }
+    else if (missing(c.Adjust) | c.Adjust==0 | mean(df.3==0) & c.Adjust==0){
+      c.Adjust = 0;
+      print(' Cell Size : 0 entery, set to  5 (default) ')
+    }
+    else{
+      print(' Correct Entery')
+    }
+
+    print(' Step4: Preparing for Plot ')
+
+
+    #####################  Generat Polar Plot #####################
+    p <- plot_ly(
+      type = 'scatterpolargl',
+      r = c((df$v5)-30, df$v4+off_set),
+      mode = 'markers_line'
+    )
+
+    ######################## Zero-line Plot ######################
+    p <- add_trace(
+      p,
+      mode= 'lines+markers',
+      r = as.list(rep(0, 360)),
+      theta = c(seq(0, 360, by=1)),
+      line = list(
+        size = 1,
+        color=  'rgb(10,10,10)'  ),
+      marker = list(
+        color = 'rgb(10,10,10)',
+        size = 2,
+        line = list(
+          color = 'black'
+        ),
+        opacity = 0.5
       )
+    )
 
-      ######################## Zero-line Plot ######################
+    ######################## Distance Plot ######################
+    j = 1
+    k = 2
+
+    for (i in 1:(length(df.1)/2)){
+
+      ################### Adjust Cell Size #########################
+
+      if (c.Adjust==0){
+        cellSize = 5
+      }
+      else{
+        cellSize= df.3[i]/max.area+c.Adjust
+      }
+
+      ####################### Color Condition #####################
+      if(df.2[i] == 0){
+        c=   'rgb(255, 255, 255)'  #     no cell
+      }
+      else if (df.2[i] == 1){
+        c=  'rgb(10, 10, 10)'      #     1 cells
+      }
+      else if (df.2[i] == 2){
+        c=  'rgb(120, 150, 237)'   #    2 cells
+      }
+      else if (df.2[i] == 3){
+        c = 'rgb(219,120,147)'     #     3 cells
+      }
+      else if (df.2[i] == 4 ){
+        c=  'rgb(10, 140, 30)'     #     4 cells
+      }
+      else if (df.2[i] == 5 ){
+        c=  'rgb(148, 10, 180)'    #     5 cells
+      }
+      else {
+        c=  'rgb(220, 180, 10)'    #   above 5 cells
+      }
+
       p <- add_trace(
         p,
         mode= 'lines+markers',
-        r = as.list(rep(0, 360)),
-        theta = c(seq(0, 360, by=1)),
+        r = df.1[,j],
+        theta = df.1[,k],
+        name = paste('Tm:',i,'\n','Ce:',df.2[i]),  # tm  : Time Frame , Cell : Number of Cells
         line = list(
-          size = 1,
-          color=  'rgb(10,10,10)'  ),
+          size = 0.2,
+          color= c),
         marker = list(
-          color = 'rgb(10,10,10)',
-          size = 2,
+          color =  c,
+          size = cellSize,
           line = list(
-            color = 'black'
+            color = c
           ),
-          opacity = 0.5
-        )
+          opacity = 10,
+          showlegend = F,
+          line = c)
       )
 
-      ######################## Distance Plot ######################
-      j = 1
-      k = 2
-
-      for (i in 1:(length(df.1)/2)){
-
-        ################### Adjust Cell Size #########################
-
-        if (c.Adjust==0){
-          cellSize = 5
-        }
-        else{
-          cellSize= df.3[i]/max.area+c.Adjust
-        }
-
-        ####################### Color Condition #####################
-        if(df.2[i] == 0){
-          c=   'rgb(255, 255, 255)'  #     no cell
-        }
-        else if (df.2[i] == 1){
-          c=  'rgb(10, 10, 10)'      #     1 cells
-        }
-        else if (df.2[i] == 2){
-          c=  'rgb(120, 150, 237)'   #    2 cells
-        }
-        else if (df.2[i] == 3){
-          c = 'rgb(219,120,147)'     #     3 cells
-        }
-        else if (df.2[i] == 4 ){
-          c=  'rgb(10, 140, 30)'     #     4 cells
-        }
-        else if (df.2[i] == 5 ){
-          c=  'rgb(148, 10, 180)'    #     5 cells
-        }
-        else {
-          c=  'rgb(220, 180, 10)'    #   above 5 cells
-        }
-
-        p <- add_trace(
-          p,
-          mode= 'lines+markers',
-          r = df.1[,j],
-          theta = df.1[,k],
-          name = paste('Tm:',i,'\n','Ce:',df.2[i]),  # tm  : Time Frame , Cell : Number of Cells
-          line = list(
-            size = 0.2,
-            color= c),
-          marker = list(
-            color =  c,
-            size = cellSize,
-            line = list(
-              color = c
-            ),
-            opacity = 10,
-            showlegend = F,
-            line = c)
-        )
-
-        if(df.2[i]==1){
-          rls=rbind(rls,i)
-        }
-
-        j <- j + 2
-        k <- k + 2
-
+      if(df.2[i]==1){
+        rls=rbind(rls,i)
       }
 
-      ######################## RLS Plot (out layer) ######################
-      # p <- add_trace(
-      #     p,
-      #     mode= 'markers',
-      #     r = as.list(rep(df$v4+off_set, 360)),
-      #     theta = c(rls),
-      #     marker = list(
-      #       color = 'rgb(10,10,10)',
-      #        symbol= "star",
-      #       size = 8,
-      #       opacity = 10)
-      #     )
-      ###################################################################
-      print(' Step4: Preparing for layout  >>>>>>>>>>>>>>>>>>>>>>>>>')
+      j <- j + 2
+      k <- k + 2
 
-      # Plot layout features
-
-      p <- layout(
-        p,
-        title =  paste('--------------------------- :', 0 ),
-        showlegend = T,
-        paper_bgcolor = "rgb(256, 256, 256)",  #  outside color
-        polar = list(
-          bgcolor = "rgb(230, 230, 230)",     #   polar background color
-          angularaxis = list(
-            tickwidth = 3,
-            linewidth = 2,
-            layer = 'below traces'
-
-          ),
-          radialaxis = list(
-            showline = T,
-            linewidth = 1,
-            tickwidth = 1,
-            gridcolor = 'rgb(256,256,256)',
-            gridwidth = 0.3
-          )
-        )
-      )
     }
 
+    ######################## RLS Plot (out layer) ######################
+    # p <- add_trace(
+    #     p,
+    #     mode= 'markers',
+    #     r = as.list(rep(df$v4+off_set, 360)),
+    #     theta = c(rls),
+    #     marker = list(
+    #       color = 'rgb(10,10,10)',
+    #        symbol= "star",
+    #       size = 8,
+    #       opacity = 10)
+    #     )
+    #
+    print(' Step5: Preparing for layout ')
 
-      ###############################
-      #######  t2d function Argument :
+    # Plot layout features
 
-      # 0 : data
-      # 1 : time col , time colunm number in dataset
-      # 2 : num_images, Number of images to visulize
+    p <- layout(
+      p,
+      title =  paste('Add your image number :', 0 ),
+      showlegend = T,
+      paper_bgcolor = "rgb(256, 256, 256)",  #  outside color
+      polar = list(
+        bgcolor = "rgb(230, 230, 230)",     #   polar background color
+        angularaxis = list(
+          tickwidth = 3,
+          linewidth = 2,
+          layer = 'below traces'
 
-      df.t2d = t2d(df,1,num_images)
+        ),
+        radialaxis = list(
+          showline = T,
+          linewidth = 1,
+          tickwidth = 1,
+          gridcolor = 'rgb(256,256,256)',
+          gridwidth = 0.3
+        )
+      )
+    )
+  }
 
-      ###############################
-      ###############  c2r Argument :
+  ############# Calling functions #################
+  # 0 : Dataset
+  # 1 : time col
+  # 2 : num_images
 
+  df.t2d = t2d(df,1,num_images)
 
-      # 0 = data , data from t2d function
-      # 1 : time col , time colunm number in dataset
-      # 2 : obj_col, total total cell colunm number in dataset
-      # 3 : area_col, area colunm number in dataset, if the area is unavailabl, set to zero (0 )
-      # 4 : dist_col  distance colunm number in dataset
-      # 5 : theta_col , theta colunm number from t2d function, default set to 5
-      # 6 : num_images, Number of images to visulize
+  # 0 = data from t2d function
+  # 1 : time col
+  # 2 : obj_col
+  # 3 : area_col
+  # 4 : dist_col
+  # 5 : theta_col
+  # 6 : num_images
 
+  df.c2r <- c2r(df.t2d,1,2,3,4,5,num_images)
 
-      df.c2r <- c2r(df.t2d,1,2,3,4,5,num_images)
+  # 0 = data
+  # 1 : offset raduis from outlayer ( Default = 0 )
+  # 2 : Adjust cell size to be desplay with actuall zise or defult = 0
 
+  p<- p2p(df.c2r,offset,c.Adjust)
 
-      ###############################
-      ######  mupolar Argument :
-
-      # 0 : df.c2r, data from c2r function
-      # 1 : offset,  raduis from out-layer ( Default = 0 )
-      # 2 : c.Adjust, cell size to be display with actuall size or if the area is unavailabl, set to zero (0 )
-
-      p<- ipolar(df.c2r,offset,c.Adjust)
-
-      p
-
+  p
 }
 #############################################################################################
 ############################### How to run the function from here ###########################
 #############################################################################################
 
 
-###################################### Load data #############################################
+################ Load data ##################
 
 #df = read.csv("../data/BC8_Tp1.csv")
 #df= select(df,"time_num","total_objs","Est_area","dist")  #arrange feature in order
 
-##############################  uPolar Function  Arguments ###################################
-
+#######  uPolar Function  Arguments ##########
 
 ##### Data format (df, time_col,objs_col,area_col,dist_col,offset, c.Adjust ,num_images)######
-##### Data format (0 ,   1,        2,        3,      4,      5,        6,        7     )######
+##### Data format (0 ,   1,        2,        3,      4,    offset, c.Adjust ,num_images)######
 
 
 # 0 : df, dataset
